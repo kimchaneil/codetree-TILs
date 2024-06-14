4a3380cfd6379a7b2961da461101fbb996d47532
kimchaneil <102949361+kimchaneil@users.noreply.github.com>
2024-06-15 00:29:30 +0900

    Title: 살 수 있는 물건 2, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240615/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -0,0 +1,11 @@
+a= int(input())
+if a>=500:
+    if a>=1000:
+        if a>=3000:
+            print('book')
+        else:
+            print('mask')
+    else:
+        print('pen')
+else:
+    print('no')
